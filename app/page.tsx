7ae9e95f4971af804d14e6db0d608e58759a48be
Robin Shen <robin@onedev.io>
2025-07-22 15:01:12 +0800

    Add greeting message to home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-2xl font-bold text-center sm:text-left">Hi World</h1>
+        <p className="text-lg text-center sm:text-left">hello robin</p>
         <Image
           className="dark:invert"
           src="/next.svg"
